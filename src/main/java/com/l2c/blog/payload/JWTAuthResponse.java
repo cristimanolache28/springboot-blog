5f5dd72ffdec89390623aa6425836a16e92ed268
Cristian Manolache <cristianmanolache0@gmail.com>
2023-10-05 18:11:53 +0300

    add JwtAuthResponse DTO and configure Jwt in Spring Security

---
+++ src/main/java/com/l2c/blog/payload/JWTAuthResponse.java
@@ -0,0 +1,16 @@
+package com.l2c.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTAuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+}
